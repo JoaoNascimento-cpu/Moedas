5ab68f8cceb01416ddd6d9269e98fed005ef0731
João Nascimento <jhonyzito4gb@gmail.com>
2022-06-14 23:00:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposMoedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Tipo para Moeda*/
+
+            Console.Clear();
+
+            decimal valor = 10.25m;
+
+            Console.WriteLine(valor);
+        }
+    }
+}

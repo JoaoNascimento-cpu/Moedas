7687e4373c5f6badd763f987c20bc68f349d5f15
João Nascimento <jhonyzito4gb@gmail.com>
2022-06-14 23:20:58 -0300

    utilizando o 'math'

--- Program.cs
+++ Program.cs
@@ -13,10 +13,14 @@ namespace TiposMoedas
 
             decimal valor = 1123.2m;
 
-            Console.WriteLine(valor.ToString("p",CultureInfo.CreateSpecificCulture("pt-BR")));//porcentagem
-            Console.WriteLine(valor.ToString("N",CultureInfo.CreateSpecificCulture("pt-BR")));//formata para moeda só que sem a sigla 
-            Console.WriteLine(valor.ToString("f",CultureInfo.CreateSpecificCulture("pt-BR")));//adiciona casa decimal, aumentando a precisão
-            Console.WriteLine(valor.ToString("C",CultureInfo.CreateSpecificCulture("pt-BR")));//padrão moeda
+            // Console.WriteLine(valor.ToString("p",CultureInfo.CreateSpecificCulture("pt-BR")));//porcentagem
+            // Console.WriteLine(valor.ToString("N",CultureInfo.CreateSpecificCulture("pt-BR")));//formata para moeda só que sem a sigla 
+            // Console.WriteLine(valor.ToString("f",CultureInfo.CreateSpecificCulture("pt-BR")));//adiciona casa decimal, aumentando a precisão
+            // Console.WriteLine(valor.ToString("C",CultureInfo.CreateSpecificCulture("pt-BR")));//padrão moeda
+
+            Console.WriteLine(Math.Round(valor)); //arredonda o valor
+            Console.WriteLine(Math.Ceiling(valor)); //arreonda para cima
+            Console.WriteLine(Math.Floor(valor)); //arredonda o valor para baixo
         }
     }
 }
